e66d4160e639c8db6b33d90ef46208b0ad1b15fb
Konstantin Koval <konstantin@rocketfarm.no>
2014-12-19 11:31:28 +0100

    Change file helper

--- Sweet/Source/FileHelper.swift
+++ Sweet/Source/FileHelper.swift
@@ -13,16 +13,39 @@ enum DirectoryType {
 
 public class FileHelper {
   
-  public class var documentDirectory: NSURL {
+  public class var documentDirectory: String {
     return directoryPath(.DocumentDirectory)
   }
   
-  public class func filePathURL(file :String, directory: NSSearchPathDirectory = .DocumentDirectory) -> NSURL {
-    return directoryPath(directory).URLByAppendingPathComponent(file);
+  public class func filePath(file :String, directory: NSSearchPathDirectory = .DocumentDirectory) -> String {
+    return directoryPath(directory).stringByAppendingPathComponent(file)
   }
   
-  private class func directoryPath(dir: NSSearchPathDirectory) -> NSURL {
-    let urls = NSFileManager.defaultManager().URLsForDirectory(dir, inDomains: .UserDomainMask)
-    return urls.last! as NSURL
+  public class func getfilePath(fileName :String, directory: NSSearchPathDirectory = .DocumentDirectory) -> NSString {
+    //let path = directoryPath(directory).URLByAppendingPathComponent(fileName).absoluteString!
+
+    var error: NSError?
+//    if !(NSFileManager.defaultManager().fileExistsAtPath(path)) {
+//      let res = NSFileManager.defaultManager().createDirectoryAtPath(path, withIntermediateDirectories: true, attributes: nil, error: &error)
+//      println(res)
+//    }
+    
+    let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as NSArray
+    let documentsDirectory = paths.objectAtIndex(0) as NSString
+    let path = directoryPath(directory)
+    
+    let path1 = documentsDirectory.stringByAppendingPathComponent("MyFile.plist")
+    
+    error = nil
+    if !(NSFileManager.defaultManager().fileExistsAtPath(path1)) {
+      let res = NSFileManager.defaultManager().createDirectoryAtPath(path1, withIntermediateDirectories: true, attributes: nil, error: &error)
+      println(res)
+    }
+
+    return ""
+  }
+  
+  private class func directoryPath(dir: NSSearchPathDirectory) -> String {
+    return NSSearchPathForDirectoriesInDomains(dir, .UserDomainMask, true).first as String
   }
 }
