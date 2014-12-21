1935cd5bfb699135e34566cbcd60502a3c192b42
Konstantin Koval <konstantin@rocketfarm.no>
2014-12-21 21:58:15 +0100

    Add shared container path

--- Sweet/Source/FileHelper.swift
+++ Sweet/Source/FileHelper.swift
@@ -21,6 +21,19 @@ public class FileHelper {
     return directoryPath(directory).stringByAppendingPathComponent(file)
   }
   
+  public class func sharedApplicationGroupContainer(groupName: String) -> NSURL {
+    if let containerURL = NSFileManager.defaultManager().containerURLForSecurityApplicationGroupIdentifier(groupName) {
+      return containerURL
+    }
+    fatalError("The shared application group container is unavailable. Check your entitlements and provisioning profiles for this target")
+  }
+  
+  public class func sharedFilePath(groupName: String)(file :String) -> NSURL {
+    return sharedApplicationGroupContainer(groupName).URLByAppendingPathComponent(file)
+  }
+
+
+//  MARK:- Private
   private class func directoryPath(dir: NSSearchPathDirectory) -> String {
     return NSSearchPathForDirectoriesInDomains(dir, .UserDomainMask, true).first as String
   }
